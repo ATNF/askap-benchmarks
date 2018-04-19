2c397f624328df238d4f3b2438499ba719b3edcb
Steve Ord <steve.ord@gmail.com>
2018-04-19 10:31:21 +0800

    some changes to tMajorACC - it is not compiling

---
+++ tMajorACC/pgi_setup.sh
@@ -0,0 +1,15 @@
+module purge
+module load broadwell
+module load gcc
+module load fftw
+module load ant
+module load scons
+module load java
+module load python/2.7.13
+
+module unload gcc
+module load pgi/18.3
+module load intel-mpi
+module load intel-mpi-wrappers
+
+
