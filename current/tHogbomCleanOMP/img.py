a22899654a6f39d5c9291df06daa24a2726e561e
damitch <daniel.mitchell@csiro.au>
2019-10-24 12:27:42 +1100

    fixed bug in img.py scripts

--- current/tHogbomCleanOMP/img.py
+++ current/tHogbomCleanOMP/img.py
@@ -2,13 +2,14 @@
 import sys
 from pylab import *
 
-# data are 4096x4096 32-bit floats
+# data are dim x dim 32-bit floats
 dfile = sys.argv[1]
 
 print 'loading image', dfile
 im = np.fromstring(file(dfile, 'rb').read(), np.float32).astype(float)
-dim = sqrt(im.size)
+dim = int(np.sqrt(im.size))
 im.shape = dim, dim
+print "image shape is", im.shape
 
 imshow(im, cmap=cm.jet)
 axis('off')
