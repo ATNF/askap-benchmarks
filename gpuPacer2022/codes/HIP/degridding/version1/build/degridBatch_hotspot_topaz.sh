6dd0475b4d1e1e0c69201da579d74e6c601d656e
Joe Schoonover <joe@fluidnumerics.com>
2022-09-13 08:08:38 +0800

    Add hotspot script for Mulan

--- gpuPacer2022/codes/HIP/degridding/version1/build/degridBatch_hotspot_topaz.sh
+++ gpuPacer2022/codes/HIP/degridding/version1/build/degridBatch_hotspot_topaz.sh
@@ -7,9 +7,13 @@
 ###SBATCH --account=director2196
 #SBATCH --account=pawsey0007
 
-module load hip/4.3.0 cuda/11.4.2 gcc/11.1.0
+module load hip/4.3.0 
+module load cuda/11.4.2 gcc/11.1.0
 
+export CUDA_PATH=$CUDA_HOME
+echo $CUDA_HOME
 make clean
+#CXXFLAGS="-v -Xcompiler -fopenmp -O2 -arch sm_72" make all
 CXXFLAGS="-v -Xcompiler -fopenmp -O2" make all
 
 srun nvprof ./bin/askapDegrid.exe
