be572ba348aedd7689142aea7b5e6cd3207ad276
Pascal Elahi <pascaljelahi@gmail.com>
2022-09-15 11:14:27 +0800

    Minor update to error reporting

--- gpuPacer2022/codes/HIP/clean/version1/utilities/MaxError.h
+++ gpuPacer2022/codes/HIP/clean/version1/utilities/MaxError.h
@@ -9,4 +9,6 @@ class MaxError
 {
 public:
     void maxError(const std::vector<T>& v1, const std::vector<T>& v2) const;
+    void reportError(size_t size, double aveErr, double frac_non_zero, double maxErr, int index) const;
+    void reportError(size_t size, double aveErr, double frac_non_zero, double maxErr_r , double maxErr_i, int index) const;
 };
