d903efa6b84ff31aebf4286c37c04f4bdf4de37d
ocekmer <ozgurcekmer@gmail.com>
2022-09-15 09:40:45 +0930

    Fix 4096x4069 case bug on CLEAN

--- gpuPacer2022/codes/CUDA/clean/version4/src/HogbomGolden.cpp
+++ gpuPacer2022/codes/CUDA/clean/version4/src/HogbomGolden.cpp
@@ -100,7 +100,7 @@ void HogbomGolden::deconvolve(const std::vector<float>& dirty,
 		}
 
 		// Check if the threshold is reached
-		if (abs(absPeakVal) < gThreshold) 
+		if (fabs(absPeakVal) < gThreshold) 
 		{
 			cout << "Reached stopping threshold" << endl;
 			break;
